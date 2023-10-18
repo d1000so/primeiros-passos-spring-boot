00d6b299165ce5628b877be7a2b6fa3ccda2503a
edmilsonbraga <d1000so@unicamp.br>
2023-10-18 01:07:57 -0300

    first commit

---
+++ src/main/java/dio/primeirospassosspringboot/PrimeirosPassosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dio.primeirospassosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirosPassosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirosPassosSpringBootApplication.class, args);
+	}
+
+}
